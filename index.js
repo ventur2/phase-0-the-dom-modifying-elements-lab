390e4d044361d3426cede3b8c8aa721454f2394c
rachell <dazzlemevanity@gmail.com>
2022-06-12 21:37:34 -0400

    commit tree fe4488e65f873c2886f8817ab30bca7bff341a49

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector('main').remove();
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Rachell is the champion'
